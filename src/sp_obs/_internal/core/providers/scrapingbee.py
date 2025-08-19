4320394123b8c68adbbf8b75b9caa78de4019a51
Taine de Buys <tainedebuyswork@Taines-MacBook-Pro.local>
2025-08-19 19:23:01 +0100

    feat/first attempt at new scrapingbee integration

--- src/sp_obs/_internal/core/providers/scrapingbee.py
+++ src/sp_obs/_internal/core/providers/scrapingbee.py
@@ -7,14 +7,25 @@ from sp_obs._internal.core.providers.base import BaseProvider
 class ScrapingBeeProvider(BaseProvider):
     """Provider for ScrapingBee API"""
 
-    def parse_response_attributes(self, response_attributes: dict[str, Any]) -> dict[str, Any]:
+    def parse_response_headers(self, response_attributes: dict[str, Any]) -> dict[str, Any]:
         """
         Parses and processes the response attributes by removing unnecessary fields.
         """
-        if response_attributes and response_attributes.get('cost'): 
-            return {"cost" : response_attributes.get('cost')}
         
-        return response_attributes;
+        cost = response_attributes.get("Spb-cost")
+
+        if cost is not None:
+            return {"cost" : cost}
+        
+        return {} 
+
+    def parse_response_attributes(self, response_attributes: dict[str, Any]) -> dict[str, Any]: 
+        """
+        Parses and processes the response attributes by removing unnecessary fields.
+        """
+        
+        return {} 
+        
 
    
 
