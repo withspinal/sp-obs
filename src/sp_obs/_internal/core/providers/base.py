5a2d6eca75067ae023902c8d150ce6b44d9750c1
Andrew van Rensburg <andrewvanrensburg@Andrews-MBP-2.lan>
2025-10-17 17:36:53 +0100

    Add request headers

--- src/sp_obs/_internal/core/providers/base.py
+++ src/sp_obs/_internal/core/providers/base.py
@@ -10,6 +10,9 @@ class BaseProvider(metaclass=ABCMeta):
     def parse_response_attributes(self, response_attributes: dict[str, Any]) -> dict[str, Any]:
         pass
 
+    def parse_request_attributes(self, request_attributes: dict[str, Any]) -> dict[str, Any]:
+        return request_attributes
+
     def parse_response_headers(self, _: dict[str, str]) -> dict[str, str]:
         """By default, no headers are parsed"""
         return {}
--- src/sp_obs/_internal/core/providers/vertexai.py
+++ src/sp_obs/_internal/core/providers/vertexai.py
@@ -9,6 +9,12 @@ logger = logging.getLogger(__name__)
 class VertexAIProvider(BaseProvider):
     """Provider for GCP Vertex AI API"""
 
+    def parse_request_attributes(self, request_attributes: dict[str, Any]) -> dict[str, Any]:
+        request_attributes.pop("document", None)  # For Mistral OCR content
+        request_attributes.pop("messages", None)  # For Mistral chat completion
+        request_attributes.pop("response_format", None)  # For Mistral chat completion
+        return request_attributes
+
     def parse_response_attributes(self, response_attributes: dict[str, Any]) -> dict[str, Any]:
         response_attributes.pop("candidates", None)
         response_attributes.pop("candidatesTokensDetails", None)
